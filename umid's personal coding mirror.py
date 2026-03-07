0aae5df744979c5866137a8d5770520b2a1f9328
darkshark <darkshark0237@gmail.com>
2026-03-07 12:44:59 +0900

    my personal calculator

--- umid's personal coding mirror.py
+++ umid's personal coding mirror.py
@@ -1,1 +1,19 @@
-print("hello world !")
+import time
+print("welcome to my world ")
+db=(input("are you ready to star the calculator ? "))
+if db=='yes':
+ print("please , wait  for 3 seconds ! ")
+time.sleep(3)
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=(input("enter + - * /:"))
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
+elif c == '/':
+    print(a / b)
+else :
+    print("Invalid Operation !")
